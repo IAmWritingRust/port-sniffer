f5d6801f52260b2235851c570560bbfc15c52d98
FitnessDoge <930763179@qq.com>
2023-10-31 21:32:21 +0800

    Add explicit type annotations

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::sync::mpsc::{channel, Sender};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 
 pub struct Sniffer {
@@ -20,8 +20,8 @@ impl Sniffer {
         args.next(); // skip program name
         let args: Vec<_> = args.collect();
 
-        let ip_address;
-        let thread_num;
+        let ip_address: &String;
+        let thread_num: usize;
 
         if args.len() == 1 {
             // *.exe <ip_address>
@@ -60,9 +60,9 @@ impl Sniffer {
 
         // scan
         for _ in 0..self.thread_num {
-            let ip_address = Arc::new(self.ip_address);
-            let new_tx = tx.clone();
-            let clone_current_port = Arc::clone(&current_port_number);
+            let ip_address: Arc<IpAddr> = Arc::new(self.ip_address);
+            let new_tx: Sender<u16> = tx.clone();
+            let clone_current_port: Arc<Mutex<u32>> = Arc::clone(&current_port_number);
             thread::spawn(|| scan(ip_address, new_tx, clone_current_port));
         }
         drop(tx);
@@ -80,11 +80,11 @@ impl Sniffer {
 fn scan(ip_address: Arc<IpAddr>, tx: Sender<u16>, current_port: Arc<Mutex<u32>>) {
     static MAX_PORTS_NUMBER: u32 = 65535;
     loop {
-        let mut port = current_port.lock().unwrap();
+        let mut port: MutexGuard<'_, u32> = current_port.lock().unwrap();
         if *port > MAX_PORTS_NUMBER {
             break;
         }
-        let c_port = *port as u16;
+        let c_port: u16 = *port as u16;
         *port += 1; // next port
         drop(port); // manual release lock
 
